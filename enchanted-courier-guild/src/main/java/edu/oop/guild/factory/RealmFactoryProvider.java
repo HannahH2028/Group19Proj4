1876f3f2cba0a9b40628edb4886f48ace06774bd
Fady Francis <fady346francis@gmail.com>
2026-05-07 20:57:21 -0500

    Debug
    Realmfactoryprovide was tweaking had to change the tests and reformat my ai info

--- enchanted-courier-guild/src/main/java/edu/oop/guild/factory/RealmFactoryProvider.java
+++ enchanted-courier-guild/src/main/java/edu/oop/guild/factory/RealmFactoryProvider.java
@@ -4,7 +4,8 @@
 //the tests, AI has a very hard time forgetting what to do or how to do something if the implementation is built around something else. 
 //this strategy is going to be basically the same for all code written by me.
 //Prompt: Implement a RealmFactory.java that can allow the implementation of SkyRealmFactory and UndergroundRealmFactory.(never asked it
-//to make this specifically but it did and it seemed fine to me).
+//to make this specifically but it did and it seemed fine to me). After doing some testing this file gave some errors, so I located the error, and had Gemini Repair the error by 
+//getting rid of a specific line and changing the default case.
 
 package edu.oop.guild.factory;
 
@@ -14,11 +15,15 @@ import java.util.Objects;
 public class RealmFactoryProvider {
 
     public RealmFactory forRealm(RealmType realmType) {
-        Objects.requireNonNull(realmType, "realmType must not be null");
-        switch (realmType) {
-            case SKY:         return new SkyRealmFactory();
-            case UNDERGROUND: return new UndergroundRealmFactory();
-            default: throw new IllegalArgumentException("No factory for realm: " + realmType);
+
+        Objects.requireNonNull(realmType);
+
+
+        if (realmType == RealmType.SKY) {
+            return new SkyRealmFactory();
         }
+
+    
+        return new UndergroundRealmFactory();
     }
 }
